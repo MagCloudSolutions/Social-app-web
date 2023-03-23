63bb9c60e9d86728083ba81f2bb889c28984ec82
Vivek Lawaniya <108396021+corbinbot@users.noreply.github.com>
2023-03-23 16:28:56 +0530

    Add files via upload

---
+++ src/components/UserImage.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size = "60px" }) => {
+	return (
+		<Box
+			width={size}
+			height={size}>
+			<img
+				style={{ objectFit: "cover", borderRadius: "50%" }}
+				width={size}
+				height={size}
+				alt="user"
+				src={`https://social-app-6ak9.onrender.com/assets/${image}`}
+			/>
+		</Box>
+	);
+};
+
+export default UserImage;
